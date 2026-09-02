73b48ec10d47a01711688fb41882244c56e5eb60
Reichell Stacy Ardiaca Lázaro <reichell.ardiaca1@gmail.com>
2026-09-02 22:10:51 +0000

    Actualiza identidad de la aplicación a V-OPS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mi-proyecto',
+  appId: 'cl.vops.app',
+  appName: 'V-OPS',
   webDir: 'www'
 };
 
